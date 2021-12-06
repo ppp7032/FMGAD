15f9d9d7eef2fd071dd8888ba72cc8ae3c3dd4c7
ppp7032 <kamilmajewski@rocketmail.com>
2021-12-06 15:39:16 +0000

    Made the Text class be able to be given a rotation

--- core/src/com/graph/algorithms/Text.java
+++ core/src/com/graph/algorithms/Text.java
@@ -5,21 +5,25 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class Text extends Actor {
     private final String toPrint;
     private final BitmapFont font;
     private final float[] colour;
+    private final float width;
+    private final float height;
     private float x;
     private float y;
+    private float rotationDegree;
 
-    public Text(final String input, final float x, final float y, final BitmapFont font, final float[] colour, final int alignX, final int alignY) { //align -1 means left, 0 means centre, 1 means right
+    public Text(final String input, final float x, final float y, final BitmapFont font, final float[] colour, final int alignX, final int alignY, final float rotationDegree) { //align -1 means left, 0 means centre, 1 means right
         this.font = font;
         toPrint = input;
         final GlyphLayout glyphLayout = new GlyphLayout(font, toPrint);
-        float width = glyphLayout.width;
-        float height = glyphLayout.height;
+        width = glyphLayout.width;
+        height = glyphLayout.height;
         this.colour = colour;
         switch (alignX) {
             case -1:
@@ -43,7 +47,7 @@ public class Text extends Actor {
                 this.y = y;
                 break;
         }
-
+        this.rotationDegree = rotationDegree;
     }
 
     public static BitmapFont generateFont(final String fontName, final float size, final int borderWidth) {
@@ -62,8 +66,15 @@ public class Text extends Actor {
 
     @Override
     public void draw(final Batch batch, final float parentAlpha) {
+        Matrix4 matrix1 = (new Matrix4()).setToTranslation(-x - width / 2f, -y + height / 2f, 0);
+        Matrix4 matrix2 = (new Matrix4()).setToRotation(0, 0, 1, rotationDegree);
+        Matrix4 matrix3 = (new Matrix4()).setToTranslation(x + width / 2f, y - height / 2f, 0);
+        Matrix4 oldMatrix = batch.getTransformMatrix().cpy();
+        batch.setTransformMatrix(matrix3.mul(matrix2).mul(matrix1));
         font.setColor(colour[0], colour[1], colour[2], colour[3]);
         font.draw(batch, toPrint, x, y);
+        batch.setTransformMatrix(oldMatrix);
+        //rotationDegree += 1;
     }
 
     @Override
