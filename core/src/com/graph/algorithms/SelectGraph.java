b3c69b40da62323850fa78f026b77954ff9de1a9
ppp7032 <kamilmajewski@rocketmail.com>
2021-12-11 15:25:43 +0000

    Added final declarations to variables declared in the prior commit.

--- core/src/com/graph/algorithms/SelectGraph.java
+++ core/src/com/graph/algorithms/SelectGraph.java
@@ -82,9 +82,9 @@ public class SelectGraph implements Screen {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 Gdx.files.local("graphs/" + graphSelector.getSelected() + ".graph2").delete();
-                Object[] currentItems = graphSelector.getItems().items;
-                ArrayList<String> newItems = new ArrayList<>();
-                for (Object o : currentItems) {
+                final Object[] currentItems = graphSelector.getItems().items;
+                final ArrayList<String> newItems = new ArrayList<>();
+                for (final Object o : currentItems) {
                     if (o != null) {
                         newItems.add((String) o);
                     }
